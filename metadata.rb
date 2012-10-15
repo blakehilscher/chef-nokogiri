4fc3e1c130bb323a5b6e6176c4c2c80872f3b510
Blake Hilscher <blake@hilscher.ca>
2012-10-15 14:39:12 -0400

    init

---
+++ metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Blake Hilscher"
+maintainer_email "blake@hilscher.ca"
+license          "All rights reserved"
+description      "Cookbook for installing nokogiri depedencies."
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.1.0"
